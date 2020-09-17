7c68cc86d37379b2a0076de90aabad46b4681604
Guilherme Rocha <g.rocha023@gmail.com>
2020-09-17 17:55:05 -0300

    configurado o controller, rotas e iniciando o model

--- src/app.js
+++ src/app.js
@@ -1,38 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const app = express();
-
 const router = express.Router();
+
+//conecta banco de dados
+mongoose.connect ('mongodb+srv://cluster0.vojyg.azure.mongodb.net/<dbname>')
+
+//carrega as rotas
+const index = require ('./routes/index');
+const products = require ('./routes/products');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.2"
-    });
-});
-
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
-
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', index)
+app.use('/products', products);
 
 module.exports = app;
